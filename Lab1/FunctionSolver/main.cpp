3f724efbb611cc5f92d088fc6db875d519bfc339
Stas Dombrovskyi <727727stas@gmail.com>
2020-10-02 00:10:42 +0300

    Create main.cpp
    Create function that send some messages to receiver and it starts function f, g, binary.

---
+++ Lab1/FunctionSolver/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <dshow.h>
+#include "Functions/functions.h"
+#define MYMESSAGE 1
+
+bool sendMessage(char function, int result);
+
+typedef struct tagMyStruct{
+    char func;
+    int res;
+} MYSTRUCT;
+
+int main(int argc, char *argv[]) {
+    int result;
+
+    if (argc == 2)
+    {
+        std::string func = argv[0];
+        std::string X = argv[1];
+
+
+        if (func == "f")
+        {
+            result = f_func(stoi(X));
+
+            sendMessage('f', result);
+        }
+        else
+        {
+            result = g_func(stoi(X));
+
+            return sendMessage('g', result);
+        }
+    }
+    else if (argc == 3)
+    {
+        std::string f = argv[1];
+        std::string g = argv[2];
+
+        result = binary_operation(stoi(f), stoi(g));
+
+        return sendMessage('b', result);
+    }
+
+    return 0;
+}
+
+bool sendMessage(char function, int result){
+    MYSTRUCT MyStruct;
+    MyStruct.func = function;
+    MyStruct.res = result;
+
+    COPYDATASTRUCT MyCDS;
+    // Fill the COPYDATA structure
+    MyCDS.dwData = MYMESSAGE;
+    MyCDS.cbData = sizeof(MyStruct);
+    MyCDS.lpData = &MyStruct;
+
+    // Call function, passing data in &MyStruct
+    HWND hwDispatch = FindWindow( "Disp32Class", "Hidden Window" );
+    if (hwDispatch != nullptr)
+    {
+        SendMessage(hwDispatch, WM_COPYDATA, (WPARAM)(HWND) NULL, (LPARAM)(LPVOID) &MyCDS);
+        return true;
+    }
+    else
+        return false;
+}
