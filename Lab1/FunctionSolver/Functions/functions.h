edee25608ac7730c3fa4f6ed14ca9c765cddb0a4
Stas Dombrovskyi <727727stas@gmail.com>
2020-10-04 00:48:30 +0300

    Update functions.h
    Update f and g, add documentation.

--- Lab1/FunctionSolver/Functions/functions.h
+++ Lab1/FunctionSolver/Functions/functions.h
@@ -1,31 +1,61 @@
 #include <iostream>
 #include <windows.h>
+#include "demofuncs"
 
+/**
+ * Function F(x)
+ * @param x
+ *  (-infinity; -1000] f hangs
+ *  (-1000; -100] f return 0
+ *  (-100; infinity) f return (x + 1) / 3
+ * @return result
+ */
 int f_func(int x){
-    if (x == 1)
-        Sleep(6000);
-    else if (x < -1000)
-        Sleep(60000);
-    else if (x == -100)
+    if (x <= -1000)
         while(1){}
-    Sleep(8000);
+    else if ((x <= -100) && (x > -1000))
+    {
+        Sleep(4000);
+        return 0;
+    }
+
+    Sleep(5000);
 
     return ((x + 1) / 3);
 }
 
+/**
+ * Function G(x)
+ * @param x
+ *  (-infinity; -2000] g return 0
+ *  (-2000; -1000] and (0; infinity) g return (x - 1) * 4
+ *  (-1000; 0] g hangs
+ * @return result
+ */
 int g_func(int x){
-    if (x == -1)
-        Sleep(10000);
-    else if (x > 1000)
-        Sleep(40000);
-    else if (x == 100)
+    if (x <= -2000)
+    {
+        Sleep(7000);
+        return 0;
+    }
+    else if ((x <= -1000) && (x > -2000) || (x >= 100))
+    {
+        Sleep(7000);
+    }
+    else if ((x <= 0) && (x > -1000))
         while(1){}
-    Sleep(4000);
+
+    Sleep(3000);
 
     return ((x - 1) * 4);
 }
 
+/**
+ * Binary operation
+ * @param f
+ * @param g
+ * @return f*g
+ */
 int binary_operation(int f, int g){
-    Sleep(2000);
     return (f * g);
 }
