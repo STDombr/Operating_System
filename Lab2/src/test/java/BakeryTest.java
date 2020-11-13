2157725ae417f118d41f61bad9a2d8d37acce5a2
Stas Dombrovskyi <727727stas@gmail.com>
2020-11-13 11:49:46 +0200

    Update BakeryTest.java

--- Lab2/src/test/java/BakeryTest.java
+++ Lab2/src/test/java/BakeryTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class BakeryTest {
 
     int counter = 0;
-    BakeryLock lock = new BakeryLock(5);
+    BakeryLock lock = new BakeryLock(256);
 
     @Test
     public void backeryCounterTest() throws InterruptedException {
@@ -13,7 +13,7 @@ public class BakeryTest {
         Runnable counterIncrementation = () -> {
             lock.register();
 
-            for(int i = 0; i < 20000; i++){
+            for(int i = 0; i < 2000; i++){
                 lock.lock();
 
                 counter++;
@@ -25,22 +25,16 @@ public class BakeryTest {
         Thread thread1 = new Thread(counterIncrementation);
         Thread thread2 = new Thread(counterIncrementation);
         Thread thread3 = new Thread(counterIncrementation);
-        //Thread thread4 = new Thread(counterIncrementation);
-        //Thread thread5 = new Thread(counterIncrementation);
 
         thread1.start();
         thread2.start();
         thread3.start();
-        //thread4.start();
-        //thread5.start();
 
         thread1.join();
         thread2.join();
         thread3.join();
-        //thread4.join();
-        //thread5.join();
 
-        Assert.assertEquals(counter, 60000);
+        Assert.assertEquals(counter, 6000);
 
         lock.reset();
     }
